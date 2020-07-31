f633e8003eb1c5e2635c16e189d6aa1e02ea223d
Hyeonho Seo <seohho@gmail.com>
2020-07-31 15:45:49 +0900

    build: Add cross-platform build tools into the project.

--- compdeps/opusarium_compdeps.h
+++ compdeps/opusarium_compdeps.h
@@ -1,4 +1,10 @@
 #ifndef OPUSARIUM_COMPDEPS_H
 #define OPUSARIUM_COMPDEPS_H
 
+#if defined(OPUSARIUM_BUILD_COMPILER_GCC)
+#include <gcc/opusarium_gcc_common.h>
+#else /* Compiler based includes */
+#error Unsupported compiler
+#endif /* Compiler based includes */
+
 #endif /* OPUSARIUM_COMPDEPS_H */
---
+++ mktools/OpusariumLanguageDetection.cmake
@@ -0,0 +1,8 @@
+include(CheckLanguage)
+check_language(C)
+if(CMAKE_C_COMPILER)
+	enable_language(C)
+else()
+	message(FATAL_ERROR "No C support")
+endif()
+set_property(GLOBAL PROPERTY C_STANDARD 11)
