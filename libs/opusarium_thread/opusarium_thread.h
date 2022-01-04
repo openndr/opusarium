7fbcc322c321157b2279f422c0e59c49cee017d4
Hyeonho Seo <seohho@gmail.com>
2022-01-04 18:29:29 +0900

    libthread: Fix the indentation format.

--- libs/opusarium_thread/opusarium_thread.h
+++ libs/opusarium_thread/opusarium_thread.h
@@ -16,55 +16,55 @@
 typedef int (*opusarium_thread_func_t)(void *);
 
 typedef enum {
-	OPUSARIUM_THREAD_STATE_FAIL = -1,
-	OPUSARIUM_THREAD_STATE_IDLE = 0,
-	OPUSARIUM_THREAD_STATE_PREP,
-	OPUSARIUM_THREAD_STATE_TRGD,
-	OPUSARIUM_THREAD_STATE_DONE,
-	OPUSARIUM_THREAD_STATE_HALT,
+    OPUSARIUM_THREAD_STATE_FAIL = -1,
+    OPUSARIUM_THREAD_STATE_IDLE = 0,
+    OPUSARIUM_THREAD_STATE_PREP,
+    OPUSARIUM_THREAD_STATE_TRGD,
+    OPUSARIUM_THREAD_STATE_DONE,
+    OPUSARIUM_THREAD_STATE_HALT,
 } opusarium_thread_state_t;
 
-typedef struct opusarium_cache_align {
-	opusarium_thread_func_t thread_func;
-	void *thread_arg;
+typedef struct {
+    opusarium_thread_func_t thread_func;
+    void *thread_arg;
 
-	uint32_t is_avoid_mask;
-	opusarium_cpu_type_t cpu_type;
-	opusarium_cpu_mask_t affinity_mask;
+    uint_fast8_t is_avoid_mask;
+    opusarium_cpu_type_t cpu_type;
+    opusarium_cpu_mask_t affinity_mask;
 
-	uint32_t is_manual_stack;
-	union {
-		struct {
-			size_t stack_size;
-			void *stack_ptr;
-		} manual_stack_conf;
-		struct {
-			size_t stack_size;
-			opusarium_memory_perm_t stack_permission;
-		} default_stack_conf;
-	};
-} opusarium_thread_conf_t;
+    uint_fast8_t is_manual_stack;
+    union {
+        struct {
+            size_t stack_size;
+            void *stack_ptr;
+        } manual_stack_conf;
+        struct {
+            size_t stack_size;
+            opusarium_memory_perm_t stack_permission;
+        } default_stack_conf;
+    };
+} opusarium_cache_align opusarium_thread_conf_t;
 
-typedef struct opusarium_cache_align {
-	opusarium_thread_state_t thread_state;
-	opusarium_thread_func_t thread_func;
-	void *thread_arg;
-	int thread_ret;
-} opusarium_thread_executor_t;
+typedef struct {
+    opusarium_thread_state_t thread_state;
+    opusarium_thread_func_t thread_func;
+    void *thread_arg;
+    int thread_ret;
+} opusarium_cache_align opusarium_thread_executor_t;
 
-typedef struct opusarium_cache_align {
-	uint64_t thread_id;
-	uint64_t thread_envpid;
-	uint64_t thread_envtid;
-	opusarium_thread_excutor_t thread_executor;
+typedef struct {
+    uint64_t thread_id;
+    uint64_t thread_envpid;
+    uint64_t thread_envtid;
+    opusarium_thread_excutor_t thread_executor;
 
-	opusarium_cpu_mask_t affinity_mask;
-	opusarium_numa_mask_t numa_mask;
+    opusarium_cpu_mask_t affinity_mask;
+    opusarium_numa_mask_t numa_mask;
 
-	uint32_t is_manual_stack;
-	size_t stack_size;
-	void *stack_ptr;
-} opusatrium_thread_t;
+    uint_fast8_t is_manual_stack;
+    size_t stack_size;
+    void *stack_ptr;
+} opusarium_cache_align opusatrium_thread_t;
 
 extern uint64_t opusarium_cache_align opusarium_thread_next_id;
 
